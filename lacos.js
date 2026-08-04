1d3413d8549d9fdbcd5d15decbcd813aee16585c
EVELLYN SILVA DE AQUINO <26171609@edu119.local>
2026-08-04 14:09:53 -0300

    Salvando Alteração

--- lacos.js
+++ lacos.js
@@ -1,3 +1,59 @@
-for(let i =1; i <= 5; i++){
-    console.log(i);
+// for(let i =1; i <= 5; i++){
+//     console.log(i);
+// }
+
+/// numero pares
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+
+/// numeros impar
+// for(let i = 2; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+// tabuada
+//  for(let i = 1; i <= 20; i ++){
+//      console.log(`5 X ${i} = ${5 * i}`);
+//}
+
+for(let hora = 0; hora <= 23; hora ++){
+    console.log(`Hora: ${hora}`);
+}
+ 
+for(let minuto = 0; minuto <= 23; minuto ++){
+    console.log(`minuto: ${minuto}`);
+}
+
+
+// for(let hora = 0; hora <= 23; hora ++){
+
+//     for(let minuto = 0; minuto <= 23; minuto ++){
+    // console.log(`${hora}:${minuto}`);
+
+// }
+
+// for(let i = 1; i <= 10 ; i ++){
+
+//     // para o laco
+//     if(i == 5){
+//         continue;
+//          break;
+//     }
+//     console.log(i)
+// }
+
+
+let soma = 0;
+
+for( let i = 1; i <=10; i++){
+    soma += i
+}
+console.log(`soma fina e: ${soma}`)
+
+let numero = 1;
+
+while(numero <= 10){
+    console.log(numero);
+    numero++;
 }
